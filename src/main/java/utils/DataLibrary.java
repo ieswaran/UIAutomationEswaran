439066c308dfb4aa80f0c1dbd3a0981dd3916322
ieswaran <eswaran.anantharaman@gmail.com>
2023-08-09 15:41:28 +0530

    c

--- src/main/java/utils/DataLibrary.java
+++ src/main/java/utils/DataLibrary.java
@@ -10,15 +10,14 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class DataLibrary {
 
 	public static Object[][] readExcelData(String excelfileName) throws IOException {
-		XSSFWorkbook wbook = 
-				new XSSFWorkbook("./src/test/resources/data/"+excelfileName+".xlsx");
+		XSSFWorkbook wbook = new XSSFWorkbook("./src/test/resources/data/"+excelfileName+".xlsx");
 		XSSFSheet sheet = wbook.getSheetAt(0);
 		int rowCount = sheet.getLastRowNum();
 		int colCount = sheet.getRow(0).getLastCellNum();
 		Object[][] data = new Object[rowCount][colCount];
 		for (int i = 1; i <= rowCount; i++) {
 			XSSFRow row = sheet.getRow(i);
-			for (int j = 0; j < colCount; j++) {
+			for (int j = 0; j <= colCount; j++) {
 				XSSFCell cell = row.getCell(j);
 				String stringCellValue = cell.getStringCellValue();
 				data[i-1][j] = stringCellValue;
